61abc9a9e5b69d38ddc3d31de86453fbd3a789b2
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-07-18 14:28:32 -0500

    Assign variable to sum

--- Strings.py
+++ Strings.py
@@ -1,11 +1,19 @@
+
+# Assign String to a Variable
 name = "Nick"
 print(name)
-
+# Reassign String Variable
 name = "Yusuke"
 print(name)
-
+# Assign number to a Variable
 bench = 265
-
+# Add 10 to the number
 new_bench = bench + 10
 
 print(new_bench)
+
+# You cannot add a number to a string
+
+# Assign a variable to a sum
+sum = 4 + 4
+
